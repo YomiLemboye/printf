ef57c55e1893bf763edb06602773b479e1bdee9b
isaacemmanuel37 <isaacochukoemmanuel@gmail.com>
2023-10-15 11:37:30 -0700

    main.h

--- main.h
+++ main.h
@@ -109,6 +109,5 @@ int is_digit(char);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
-/*collaborators: yomilamboye isaacemmanuel37
- */
+
 #endif /* MAIN_H */
