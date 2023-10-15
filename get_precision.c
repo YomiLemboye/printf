3f41e809b8828ea0d1fe82f6c9ecf1d05500f24e
yomilemboye <lemboyeyomi@gmail.com>
2023-10-15 11:27:11 -0700

    compiled a.out

--- get_precision.c
+++ get_precision.c
@@ -5,40 +5,11 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  * @list: list of arguments.
-<<<<<<< HEAD
-=======
  *
->>>>>>> 7f9c5161d68d60f3e8e642f547dd679abe18e02e
  * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-<<<<<<< HEAD
-int curr_i = *i + 1;
-int precision = -1;
-if (format[curr_i] != '.')
-return (precision);
-precision = 0;
-for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-{
-if (is_digit(format[curr_i]))
-{
-precision *= 10;
-precision += format[curr_i] - '0';
-}
-else if (format[curr_i] == '*')
-{
-curr_i++;
-precision = va_arg(list, int);
-break;
-}
-else
-break;
-}
-*i = curr_i - 1;
-return (precision);
-}
-=======
 	int curr_i = *i + 1;
 	int precision = -1;
 
@@ -68,5 +39,3 @@ return (precision);
 
 	return (precision);
 }
-/*Collaborators: YomiLemboye and Isaacemmmanuel37/
->>>>>>> 7f9c5161d68d60f3e8e642f547dd679abe18e02e
